4a9ef0c23c5576ae1ac71240f6388f3833db259b
Bruce Lee <brucehlee7@gmail.com>
2021-11-13 22:26:21 +0000

    packing-list

---
+++ lighthouse-js-fundamentals/ice-cream.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+// var first = iceCreamFlavours[0];
+// console.log(first);
+var last = iceCreamFlavours[5];
+console.log(last);
+console.log(iceCreamFlavours.length);
